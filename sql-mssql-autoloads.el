47e5393e88d129815fbd7cf5b67a4ca3b072f4ee
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2018-02-02 14:36:10 +0100

    why was this gone from github?

---
+++ sql-mssql-autoloads.el
@@ -0,0 +1,15 @@
+;;; sql-mssql-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("sql-mssql.el") (22866 17263 655951 508000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; sql-mssql-autoloads.el ends here
---
+++ sql-mssql-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "sql-mssql" "20160512.137" "Connect Microsoft SQL Server with sql.el in Linux/Unix environment" '((emacs "24.4")) :commit "f8a7f16720fbc7480a7ae83e21958860f66c1ccf" :url "https://github.com/tumashu/sql-mssql")
